8942b347e0265d86bcc3f4acf66a57b1c280d0c1
mangonob <534393806@qq.com>
2016-03-02 20:01:40 +0800

    add .gitignore to ignore .DS_Store

--- Bomb/main.cpp
+++ Bomb/main.cpp
@@ -6,10 +6,57 @@
 //  Copyright © 2016年 Trinity. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+typedef unsigned long long ull;
+ull dp[25][3];
+
+void genDp(){
+    dp[0][0] = 1;
+    for (int i = 1; i <= 22; i++) {
+        dp[i][0] = dp[i-1][0]*10 - dp[i-1][1];
+        dp[i][1] = dp[i-1][0];
+        dp[i][2] = dp[i-1][2]*10 + dp[i-1][1];
+    }
+}
+
+
+ull solve(ull n) {
+    ull result = 0;
+    int a[25], len = 0;
+    while (n) {
+        a[++len] = n % 10;
+        n/=10;
+    }
+    a[len + 1] = 0;
+    
+    bool flag = false;
+    for (int i = len; i >= 1; i--) {
+        int curr = a[i];
+        int prev = a[i + 1];
+        if(flag) { result += curr * (dp[i - 1][0] + dp[i - 1][2]); }
+        else {
+            result += curr * dp[i - 1][2];
+            if (curr > 4) result += dp[i - 1][1];
+            if (curr == 9 && prev == 4) {
+                result++;
+                flag = true;
+            }
+        }
+    }
+    
+    return result;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    genDp();
+    int n;
+    ull v;
+    scanf("%d", &n);
+    for(;n--;) {
+        scanf("%llu", &v);
+        ull value = solve(v);
+        printf("%llu\n", value);
+    }
     return 0;
 }
